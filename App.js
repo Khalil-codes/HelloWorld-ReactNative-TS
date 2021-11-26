84a03d3485fd1a3f6160ba23d58bb05027e3f69e
khalil <khalilpati55@gmail.com>
2021-11-26 17:30:47 +0530

    Some Changes

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello World</Text>
+      <Button color="#000" title="Click Me"/>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +15,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  btn:{
+    backgroundColor:"#000",
+    color:"#fff",
   },
 });
